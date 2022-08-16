2feff592cbdc6ac662670f3b333366b65d9f617b
bdvela <bdvela20@gmail.com>
2022-08-16 12:17:31 -0500

    fix: update css

--- includes/Pokemon.php
+++ includes/Pokemon.php
@@ -13,17 +13,17 @@ $pokemons[0]->name = 'Pikachu';
 $pokemons[0]->pic = 'https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/34861-pikachu-clipart-png.png?resize=900%2C900&ssl=1';
 $pokemons[0]->ability = 'rayo';
 
-//BULBASAUR
-$pokemons[1] = new Pokemon();
-$pokemons[1]->name = 'Bulbasaur';
-$pokemons[1]->pic = 'https://www.pngmart.com/files/11/Pokemon-Bulbasaur-PNG-Clipart.png';
-$pokemons[1]->ability = 'tierra';
-
 //CHARMANDER
+$pokemons[1] = new Pokemon();
+$pokemons[1]->name = 'Charmander';
+$pokemons[1]->pic = 'https://www.pngmart.com/files/13/Charmander-PNG-HD.png';
+$pokemons[1]->ability = 'fuego';
+
+//BULBASAUR
 $pokemons[2] = new Pokemon();
-$pokemons[2]->name = 'Charmander';
-$pokemons[2]->pic = 'https://www.pngmart.com/files/13/Charmander-PNG-HD.png';
-$pokemons[2]->ability = 'fuego';
+$pokemons[2]->name = 'Bulbasaur';
+$pokemons[2]->pic = 'https://www.pngmart.com/files/11/Pokemon-Bulbasaur-PNG-Clipart.png';
+$pokemons[2]->ability = 'tierra';
 
 //CHARIZARD
 $pokemons[3] = new Pokemon();
@@ -37,7 +37,13 @@ $pokemons[4]->name = 'Blastoise';
 $pokemons[4]->pic = 'https://www.pngplay.com/wp-content/uploads/10/Blastoise-Pokemon-No-Background.png';
 $pokemons[4]->ability = 'agua';
 
-//Meowth
+//UMBREON
+$pokemons[5] = new Pokemon();
+$pokemons[5]->name = 'Umbreon';
+$pokemons[5]->pic = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png';
+$pokemons[5]->ability = 'lunar';
+
+//MEOWTH
 $pokemons[6] = new Pokemon();
 $pokemons[6]->name = 'Meowth';
 $pokemons[6]->pic = 'https://www.pngplay.com/wp-content/uploads/11/Meowth-Pokemon-Background-PNG-Image.png';
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include("includes/Pokemon.php") ?>
     <h1>Pokedex</h1>
     <?php foreach ($pokemons as $pokemon) { ?>
-    <div class="card <?php echo $pokemon -> ability ?>">
+    <div class="card <?php echo $pokemon -> ability?>">
         <img src="<?php echo $pokemon -> pic ?>" alt="<?php echo $pokemon->name ?>" class="img">
         <div class="name"><h2><?php echo $pokemon->name ?></h2></div>
         <div class="ability"><div class="chip"><p><?php echo $pokemon->ability ?></p></div></div>
